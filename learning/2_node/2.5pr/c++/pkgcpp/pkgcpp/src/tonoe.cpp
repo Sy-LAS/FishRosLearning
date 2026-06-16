1f9f81875f5e244132abeba5747f68a193866fd6
Sy-LAS <Yingkai.Shao25@student.xjtlu.edu.cn>
2026-06-17 01:07:28 +0800

    2.5.3exeable

--- learning/2_node/2.5pr/c++/pkgcpp/pkgcpp/src/tonoe.cpp
+++ learning/2_node/2.5pr/c++/pkgcpp/pkgcpp/src/tonoe.cpp
@@ -2,7 +2,7 @@
 #include <functional>
 void FreeFunction(std::string str)
 {
-    std::cout << "自由函数："str << std::endl;
+    std::cout << "自由函数：" << str << std::endl;
 }
 
 class MyClass
@@ -10,15 +10,18 @@ class MyClass
 public:
     void MemberFunction(std::string str)
     {
-        std::cout << “成员函数”str << std::endl;
+        std::cout << "成员函数" << str << std::endl;
     }
 };
 
 int main()
 {
-    auto lambda = [](std::string str) -> void { std::cout << "lambda函数："str << std::endl; };
+    auto lambda = [](std::string str) -> void { std::cout << "lambda函数：" << str << std::endl; };
     std::function<void(std::string)> free = FreeFunction;
     std::function<void(std::string)> member = std::bind(&MyClass::MemberFunction, MyClass(), std::placeholders::_1);
     std::function<void(std::string)> lambda_func = lambda;
+    free("hello");
+    member("world");
+    lambda_func("test");
     return 0;
 }
